e5756b230b361a755e0d593aa63ba6b8a57976a6
Michele Rastelli <rashtao@gmail.com>
2020-01-30 15:17:30 +0100

    next

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,9 @@
-import com.arangodb.ArangoDB;
-import com.arangodb.velocystream.Request;
-import com.arangodb.velocystream.RequestType;
-import com.arangodb.velocystream.Response;
+
+import com.arangodb.next.communication.ArangoCommunication;
+import com.arangodb.next.communication.ArangoTopology;
+import com.arangodb.next.communication.CommunicationConfig;
+import com.arangodb.next.connection.*;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
@@ -9,22 +11,34 @@ import java.util.Date;
  * @author Michele Rastelli
  */
 public class Application {
-    private static Request versionRequest = new Request("_system", RequestType.GET, "/_api/version");
 
     public static void main(String[] args) {
         long start = new Date().getTime();
-        System.out.println("hi " + new Date());
-
-        ArangoDB arango = new ArangoDB.Builder().password("test").build();
-        Response res = arango.execute(versionRequest);
-        System.out.println(res.getBody());
-        arango.shutdown();
-
-        // FIXME: deserialization of ArangoDBVersion fails
-//        String version = arango.getVersion().getVersion();
-//        System.out.println(version);
-
+        getDbVersion().block();
         long end = new Date().getTime();
-        System.out.println("elapsed time [ms]: " + (end - start));
+        System.out.println("elapsed: " + (end - start) + "ms");
     }
+
+    private static Mono<ArangoResponse> getDbVersion() {
+        final ArangoRequest REQUEST = ArangoRequest.builder()
+                .database("_system")
+                .path("/_api/version")
+                .requestType(ArangoRequest.RequestType.GET)
+                .putQueryParam("details", "true")
+                .build();
+
+        ArangoCommunication communication = ArangoCommunication.create(
+                CommunicationConfig.builder()
+                        .addHosts(HostDescription.of("localhost", 8529))
+                        .authenticationMethod(AuthenticationMethod.ofBasic("root", "test"))
+                        .topology(ArangoTopology.SINGLE_SERVER)
+                        .acquireHostList(false)
+                        .protocol(ArangoProtocol.HTTP)
+                        .build()
+        ).block();
+
+        return communication.execute(REQUEST)
+                .doOnNext(v -> System.out.println("OK"));
+    }
+
 }
